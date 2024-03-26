73fbb628c5f65fa4f1cc20f6f656b3a964b410d4
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-26 21:39:10 +0100

    updating data structure

--- src/freeCodeMapMinis/components/tree-view/menu-item.jsx
+++ src/freeCodeMapMinis/components/tree-view/menu-item.jsx
@@ -1,16 +1,15 @@
-import MenuList from "./menu-list"
-
+import MenuList from "./menu-list";
 
 export default function MenuItem({ item }) {
+  return (
+    <li>
+      <p>{item.label}</p>
 
-    return <li>
-        <p>{item.label}</p>
-
-        {
-            item && item.children && item.children.length > 0 &&
-            <ul>
-                {item.children.map((child, index) => <MenuList key={index} list={item.children} />)}
-            </ul>
-        }
+      {
+        item && item.children && item.children.length > 0
+        ? (<MenuList list={item.children} />)
+        : null
+      }
     </li>
+  );
 }
