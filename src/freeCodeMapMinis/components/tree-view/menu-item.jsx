5298aced11815ec8ae507c79de93c27d1ec81354
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-26 21:51:05 +0100

    added span marker to list items that have children

--- src/freeCodeMapMinis/components/tree-view/menu-item.jsx
+++ src/freeCodeMapMinis/components/tree-view/menu-item.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import MenuList from "./menu-list";
 
 export default function MenuItem({ item }) {
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+
   return (
     <li>
-      <p>{item.label}</p>
+        <div style={{display: 'flex', gap: '20px'}}>
+            <p>{item.label}</p>
+            {
+                item && item.children && item.children.length
+                ? <span>+</span>
+                : null
+            }
+        </div>
 
       {
         item && item.children && item.children.length > 0
