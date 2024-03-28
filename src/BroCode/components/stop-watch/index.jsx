747a69087f6e5ebae765585fafa805a8b4f5fa17
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-28 19:12:14 +0100

    pause

--- src/BroCode/components/stop-watch/index.jsx
+++ src/BroCode/components/stop-watch/index.jsx
@@ -1,33 +1,69 @@
 import { useState, useEffect, useRef } from "react";
-import './styles.css';
+import "./styles.css";
 
 function StopWatch() {
   const [isRunning, setIsRunning] = useState(false);
   const [elapsedTime, setElapsedTime] = useState(0);
-  const instervalIdRef = useRef(null);
+  const intervalIdRef = useRef(null);
   const startTimeRef = useRef(0);
 
   // we perform some code if the component is mounted
   // and if the StopWatch is running
-  useEffect(() => {}, [isRunning]);
+  useEffect(() => {
+    if (isRunning) {
+      setInterval(() => {
+        intervalIdRef.current = setElapsedTime(
+          Date.now() - startTimeRef.current
+        );
+      }, 10);
+    }
+    // clean up the effect when isRunning changes
+    // or we unmount our component
+    // we want to stop the interval continuing
+    return () => {
+      clearInterval(intervalIdRef.current);
+    };
+  }, [isRunning]);
 
-  function start() {}
+  function start() {
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
+  }
 
-  function stop() {}
+  function stop() {
+    setIsRunning(false);
+    console.log("Stopping")
 
-  function reset() {}
+  }
+
+  function reset() {
+    setElapsedTime(0);
+    setIsRunning(false);
+  }
 
   function formatTime() {
-    return "00:00:00";
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60 ));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / (1000) % 60);
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+
+    return `${minutes}:${seconds}:${milliseconds}`;
   }
 
   return (
     <div className="stopwatch">
       <div className="display">{formatTime()}</div>
       <div className="controls">
-        <button className="start-button" onClick={start}>Start</button>
-        <button className="stop-button" onClick={stop}>Stop</button>
-        <button className="reset-button" onClick={reset}>Reset</button>
+        <button className="start-button" onClick={start}>
+          Start
+        </button>
+        <button className="stop-button" onClick={stop}>
+          Stop
+        </button>
+        <button className="reset-button" onClick={reset}>
+          Reset
+        </button>
       </div>
     </div>
   );
