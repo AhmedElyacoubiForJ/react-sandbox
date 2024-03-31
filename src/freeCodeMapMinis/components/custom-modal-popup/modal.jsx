ca17343ff14a139963c1b99f3fc0624b83f32f7a
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-31 21:23:14 +0200

    modal default content

--- src/freeCodeMapMinis/components/custom-modal-popup/modal.jsx
+++ src/freeCodeMapMinis/components/custom-modal-popup/modal.jsx
@@ -1,10 +1,32 @@
-import './modal.css';
+import "./modal.css";
 
-export default function Modal() {
-
-    return (
-        <div>
-            Modal
+export default function Modal({ id, header, body, footer }) {
+  return (
+    <div id={id || "Modal"} className="madal">
+      <div className="model-content">
+        <div className="header">
+          <span className="close-modal-icon">&times;</span>
+          <h2>{header ? header : "Header"}</h2>
         </div>
-    )
+        <div className="body">
+          {body ? (
+            body
+          ) : (
+            <div>
+              <p>This is our Model Body</p>
+            </div>
+          )}
+        </div>
+        <div className="footer">
+          {footer ? (
+            footer
+          ) : (
+            <div>
+              <h2>Footer</h2>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
