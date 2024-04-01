aeda8bb5852dc54261d2fb43e1abaf27191a55d7
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-01 20:52:07 +0200

    toggle turns between players

--- src/freeCodeMapMinis/components/tic-tac-toe/index.jsx
+++ src/freeCodeMapMinis/components/tic-tac-toe/index.jsx
@@ -6,28 +6,42 @@ export default function TicTacToe() {
   // 0 1 2
   // 3 4 5
   // 6 7 8
-  const [board, setBoard] = useState(Array(9).fill(""));
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [isXTurn, setIsXTurn] = useState(true);
 
-  function Square() {
-    return <button className="square">X</button>;
+  function handleClick(index) {
+    let squaresCopy = [...squares];
+    if (squaresCopy[index] === "") {
+      squaresCopy[index] = isXTurn ? "X" : "O";
+      setSquares(squaresCopy);
+      setIsXTurn(!isXTurn);
+    }
   }
 
+  function Square({ value, onClick }) {
+    return (
+      <button onClick={onClick} className="square">
+        {value}
+      </button>
+    );
+  }
+  console.log(squares);
   return (
     <div className="tic-tac-toe-container">
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
