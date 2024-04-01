3837da129ef9d51da0a256bc868d909517bd15ee
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-01 06:18:20 +0200

    autoComplete filter

--- src/freeCodeMapMinis/components/search-autocomplete-with-api/index.jsx
+++ src/freeCodeMapMinis/components/search-autocomplete-with-api/index.jsx
@@ -1,10 +1,65 @@
-
+import { useState, useEffect } from "react";
 
 export default function SearchAutocomplete() {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [searchParam, setSearchParam] = useState("");
+  const [error, setError] = useState(null);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filterdUsers, setFilterdUsers] = useState([]);
 
-    return (
-        <div className="search-autocomplete">
-            <input type="text" placeholder="Search for a movie" />
-        </div>
-    )
+  function handleOnChange(e) {
+    const query = e.target.value.toLowerCase();
+    setSearchParam(query);
+    if (query.length > 1) {
+      const filteredData =
+        users && users.length
+          ? users.filter((item) => item.toLowerCase().indexOf(query) > -1)
+          : [];
+      setFilterdUsers(filteredData);
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false);
+    }
+  }
+
+  async function fetchListOfUsers() {
+    try {
+      setLoading(true);
+      /*  const response = await fetch(
+          `https://dummyjson.com/docs/users/search/?q=${searchParam}`
+        ); */
+      const response = await fetch(`https://dummyjson.com/users`);
+
+      const data = await response.json();
+
+      if (data && data.users && data.users.length) {
+        setUsers(data.users.map((userItem) => userItem.firstName));
+        setLoading(false);
+        setError(null);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfUsers();
+  }, []);
+
+  console.log(users, filterdUsers);
+
+  return (
+    <div className="search-autocomplete-container">
+      <input
+        onChange={handleOnChange}
+        value={searchParam}
+        type="text"
+        name="search-users"
+        placeholder="Search for users..."
+      />
+    </div>
+  );
 }
