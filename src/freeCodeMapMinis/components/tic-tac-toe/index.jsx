cde5b277d195182da7f93666883bfce4f6aa99f7
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-01 07:57:36 +0200

    UI

--- src/freeCodeMapMinis/components/tic-tac-toe/index.jsx
+++ src/freeCodeMapMinis/components/tic-tac-toe/index.jsx
@@ -1,9 +1,34 @@
-import './styles.css';
+import { useState } from "react";
+
+import "./styles.css";
 
 export default function TicTacToe() {
+  // 0 1 2
+  // 3 4 5
+  // 6 7 8
+  const [board, setBoard] = useState(Array(9).fill(""));
 
-    return (
-        <>
-        </>
-    )
+  function Square() {
+    return <button className="square">X</button>;
+  }
+
+  return (
+    <div className="tic-tac-toe-container">
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
 }
