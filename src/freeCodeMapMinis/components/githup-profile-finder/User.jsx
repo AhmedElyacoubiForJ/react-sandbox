3bb34ab0cba24aaab28872a8fa869cd6da38333e
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-01 04:23:59 +0200

    github finder done

--- src/freeCodeMapMinis/components/githup-profile-finder/User.jsx
+++ src/freeCodeMapMinis/components/githup-profile-finder/User.jsx
@@ -1,10 +1,32 @@
+export default function User({ user }) {
+  //const {login} = user;
+  const createdDate = new Date(user.created_at);
 
-
-export default function User() {
-
-    return (
-        <div>
-            <h1>User</h1>
-        </div>
-    )
+  return (
+    <div className="user">
+      <div>
+        <img src={user.avatar_url} className="avatar" alt="User" />
+      </div>
+      <div className="name-container">
+        <a href={user.html_url}>{user.name || user.login}</a>
+        <p>User joined on {`${createdDate.getDate()} ${createdDate.toLocaleString('en-us', {
+            month: 'short',
+        })} ${createdDate.getFullYear()}`}</p>
+      </div>
+      <div className="profile-info">
+      <div>
+        <p>Public Repos</p>
+        <p>{user.public_repos}</p>
+      </div>
+      <div>
+        <p>Flowers</p>
+        <p>{user.flowers}</p>
+      </div>
+      <div>
+        <p>Flowing</p>
+        <p>{user.flowing}</p>
+      </div>
+      </div>
+    </div>
+  );
 }
--- src/freeCodeMapMinis/components/githup-profile-finder/index.jsx
+++ src/freeCodeMapMinis/components/githup-profile-finder/index.jsx
@@ -1,10 +1,51 @@
-import './styles.css';
+import { useState, useEffect } from "react";
+
+import User from "./User";
+import "./styles.css";
 
 export default function GithubProfileFinder() {
+  const [userName, setUserName] = useState("AhmedElyacoubiForJ");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    return (
-        <div>
-            <h1>Github Profile Finder</h1>
-        </div>
-    )
+  function handleSubmit() {
+    fetchGithubUserData();
+  }
+
+  async function fetchGithubUserData() {
+    setLoading(true);
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await response.json();
+
+    if (data) {
+      console.log(data);
+      setUserData(data);
+      setLoading(false);
+      setUserName("");
+    }
+  }
+
+  useEffect(() => {
+    fetchGithubUserData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="github-profile-container">
+      <div className="input-wrapper">
+        <input
+          name="search-by-username"
+          type="text"
+          placeholder="Enter Github Username..."
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+      {userData !== null ? <User user={userData} /> : null}
+    </div>
+  );
 }
